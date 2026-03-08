6a7d16a1b4b0e2d6c48a5adb57c767623bec95b7
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2026-03-08 17:21:43 -0300

    FEAT: atualizando arqutietura MVC guia

---
+++ src/main/java/com/fag/lucasmartins/mvc/model/repository/impl/EventoRepositoryImpl.java
@@ -0,0 +1,39 @@
+package com.fag.lucasmartins.mvc.model.repository.impl;
+
+import com.fag.lucasmartins.mvc.model.bo.EventoBO;
+import com.fag.lucasmartins.mvc.model.repository.entity.EventoEntity;
+import com.fag.lucasmartins.mvc.model.repository.EventoRepository;
+import com.fag.lucasmartins.mvc.model.repository.jpa.EventoJpaRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class EventoRepositoryImpl implements EventoRepository {
+
+    private final EventoJpaRepository jpaRepository;
+
+    public EventoRepositoryImpl(EventoJpaRepository jpaRepository) {
+        this.jpaRepository = jpaRepository;
+    }
+
+    @Override
+    public EventoBO salvar(EventoBO bo) {
+        EventoEntity entity = new EventoEntity(bo.getId(), bo.getNome(), bo.getCapacidadeMaxima(), bo.getIngressosVendidos());
+
+        EventoEntity eventoCriado = jpaRepository.save(entity);
+
+        return new EventoBO(eventoCriado.getId(), eventoCriado.getNome(), eventoCriado.getCapacidadeMaxima(), eventoCriado.getIngressosVendidos());
+    }
+
+    @Override
+    public EventoBO buscarPorId(Long id) {
+        Optional<EventoEntity> evento = jpaRepository.findById(id);
+
+        if (evento.isEmpty()) {
+            throw new IllegalArgumentException("Evento não encontrado!");
+        }
+
+        return new EventoBO(evento.get().getId(), evento.get().getNome(), evento.get().getCapacidadeMaxima(), evento.get().getIngressosVendidos());
+    }
+}
